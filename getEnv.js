90ef0d131d8dcb6c713fb291f6e8a88f36d31e56
emperorpickles <emperor.pickles@gmail.com>
2022-02-15 21:19:33 +0000

    reading env variables from GCS

---
+++ getEnv.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const fs = require('fs');
+
+const dotEnvExists = fs.existsSync('.env');
+if (dotEnvExists) {
+    console.log('getEnv.js: .env exists');
+    process.exit();
+}
+
+const gcs = require('@google-cloud/storage')();
+
+const bucketName = `envvars-${process.env.GCLOUD_PROJECT}`;
+console.log(`Downloading .env from bucket "${bucketName}"`);
+gcs
+    .bucket(bucketName)
+    .file('.env')
+    .download({ destination: '.env' })
+    .then(() => {
+        console.info('getEnv.js: .env downloaded successfully');
+    })
+    .catch(err => {
+        console.error(`getEnv.js: There was an error: ${JSON.stringify(err, undefined, 2)}`);
+    });
